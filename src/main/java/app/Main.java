7026988a0516eb3aaaf01948073d98cd3fedd0aa
Marat Shakirov <marat@shakirov.name>
2025-01-02 16:44:22 +0300

    Add Main class

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,16 @@
 package app;
 
+import config.Config;
+
 public class Main {
+    public static void main(String[] args) {
+        Config config = new Config();
+        try {
+            config.loadFromFile("resources/config.txt");
+            App app = new App(config);
+            app.run();
+        } catch (Exception e) {
+            System.out.println("Ошибка загрузки конфигурации: " + e.getMessage());
+        }
+    }
 }

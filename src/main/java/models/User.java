78b6d7138d78826f9fca3d61ac81e0073672d122
Marat Shakirov <marat@shakirov.name>
2025-01-02 16:42:11 +0300

    Add User class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,32 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private final String name;
+    private final String uuid;
+    private final List<Link> links;
+
+    public User(String name, String uuid) {
+        this.name = name;
+        this.uuid = uuid;
+        this.links = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void addLink(Link link) {
+        links.add(link);
+    }
 }

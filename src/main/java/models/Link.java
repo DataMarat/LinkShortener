3650d1c4dc65800e6cdd0ff55ee9748f785ce739
Marat Shakirov <marat@shakirov.name>
2025-01-02 16:43:06 +0300

    Add Link class

--- src/main/java/models/Link.java
+++ src/main/java/models/Link.java
@@ -1,4 +1,35 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class Link {
+    private final String originalUrl;
+    private final String shortUrl;
+    private final int clickLimit;
+    private int clicks;
+    private final LocalDateTime expirationTime;
+
+    public Link(String originalUrl, String shortUrl, int clickLimit, LocalDateTime expirationTime) {
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+        this.clickLimit = clickLimit;
+        this.expirationTime = expirationTime;
+        this.clicks = 0;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public boolean isActive() {
+        return clicks < clickLimit && LocalDateTime.now().isBefore(expirationTime);
+    }
+
+    public void incrementClicks() {
+        clicks++;
+    }
 }
